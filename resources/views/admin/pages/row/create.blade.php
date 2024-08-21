9069bf7e60efd2708d202862c201a197eb4b126b
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2024-08-22 01:10:01 +0600

    21-08-24

--- resources/views/admin/pages/row/create.blade.php
+++ resources/views/admin/pages/row/create.blade.php
@@ -251,7 +251,7 @@
                                             <div class="col-lg-12">
                                                 <div class="d-flex justify-content-end">
                                                     <button type="submit" id="common_submit"
-                                                        class="btn btn-lg common-btn-3 fw-bolder me-4 w-175px mb-5">
+                                                        class="btn btn-primary common-btn-3 fw-bolder me-4 w-175px mb-5">
                                                         <span class="indicator-label">Submit</span>
                                                         <span class="indicator-progress">Please wait...
                                                             <span
