9fe84d110ab4c2fcd6c8c297132462475042a5e0
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2024-08-28 23:48:12 +0600

    21-08-24

--- resources/views/frontend/pages/policy/faq.blade.php
+++ resources/views/frontend/pages/policy/faq.blade.php
@@ -19,7 +19,7 @@
                         <div id="accordion">
                             <div class="border-0">
                                 @foreach ($faqs as $subKey => $faq)
-                                    <div class="card-header p-0 border-0 mb-0 mt-1" id="heading-{{ $faq->id }}">
+                                    <div class="card-header p-0 border-0 mb-2 mt-1" id="heading-{{ $faq->id }}">
                                         <button
                                             class="btn btn-link accordion-title border-0 collapse rounded-0 {{ $subKey === 0 ? 'show' : '' }}"
                                             data-toggle="collapse" data-target="#collapse-{{ $faq->id }}"
@@ -31,7 +31,7 @@
                                         </button>
                                     </div>
                                     <div id="collapse-{{ $faq->id }}"
-                                        class="collapse {{ $subKey === 0 ? 'show' : '' }}"
+                                        class="collapse {{ $subKey === 0 ? 'show' : '' }} mb-3"
                                         aria-labelledby="heading-{{ $faq->id }}" data-parent="#accordion">
                                         <div class="card-body accordion-body">
                                             <p style="font-size: 13px; font-weight: 500; text-align: justify;">
