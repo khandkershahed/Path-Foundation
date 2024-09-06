c64c789611c73bc917c9630a0e05c0f103426c97
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2024-09-06 14:29:49 +0600

    6/09/24

--- app/Models/NewsTrend.php
+++ app/Models/NewsTrend.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\Traits\HasSlug;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class NewsTrend extends Model
 {
-    use HasFactory;
-
+    use HasFactory, HasSlug;
+    protected $slugSourceColumn = 'title';
     /**
      * The attributes that aren't mass assignable.
      *
