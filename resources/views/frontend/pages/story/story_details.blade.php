d3cd13be32ec8e698a7a076003076a8279a86073
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2024-09-06 20:33:53 +0600

    6/09/24

--- resources/views/frontend/pages/story/story_details.blade.php
+++ resources/views/frontend/pages/story/story_details.blade.php
@@ -52,11 +52,11 @@
     </style>
 
     <!--======// Header Title //======-->
-    <section class="blog_header" style="background-image: url('{{ !empty($blog->banner_image) && file_exists(public_path('storage/' . $blog->banner_image)) ? asset('storage/' . $blog->banner_image) : asset('frontend/images/no-banner(1920-330).png') }}');">
+    <section class="blog_header"
+        style="background-image: url('{{ !empty($blog->banner_image) && file_exists(public_path('storage/' . $blog->banner_image)) ? asset('storage/' . $blog->banner_image) : asset('frontend/images/no-banner(1920-330).png') }}');">
         <h1 class="text-center text-white pt-5">{{ $blog->badge }}</h1>
         <div class="container ">
             <div class="row ">
-
             </div>
         </div>
     </section>
@@ -64,15 +64,6 @@
     @php
         $all_tags = $blog->tags;
         $tags = explode(',', $all_tags);
-        $industry_ids = json_decode($blog->industry_id);
-        $category_ids = json_decode($blog->category_id);
-        $brand_ids = json_decode($blog->brand_id);
-        $solution_ids = json_decode($blog->solution_id);
-        // dd($category_ids);
-        // $industries = explode(',', );
-        // $categories = explode(',', json_decode($blog->category_id));
-        // $brands = explode(',', json_decode($blog->brand_id));
-        // $solutions = explode(',', json_decode($blog->solution_id));
         $without_last_tags = array_slice($tags, 0, -1);
         $last_word = end($tags);
         // dd($without_last_tags);
@@ -86,14 +77,21 @@
                 <div class="col-lg-12 col-sm-12 shadow-lg px-5 py-3 text-center  bg-white" style="margin-top: -4.5rem; ">
                     <h1> {{ $blog->title }}</h1>
                     <div class="d-flex justify-content-between">
+
                         <p>By
-                            <span class="special_character">{{ $blog->created_by }}</span>
+                            <span
+                                class="special_character">{{ !empty($blog->created_by) ? $blog->created_by : 'PATH BANGLADESH' }}</span>
                             <span class="date_blog">/ {{ $blog->created_at->format('Y-m-d') }} /
-                            </span> Topics :
-                            @foreach ($without_last_tags as $item)
-                                <span class="special_character">{{ $item }} , </span>
-                            @endforeach
-                            <span class="special_character">{{ $last_word }} </span>
+                            </span>
+                            @if (!empty($blog->tags))
+                                Topics :
+                                @foreach ($without_last_tags as $item)
+                                    <span class="special_character"><i class="fa-regular fa-bookmark"></i>
+                                        {{ $item }} , </span>
+                                @endforeach
+                                <span class="special_character"><i class="fa-regular fa-bookmark"></i> {{ $last_word }}
+                                </span>
+                            @endif
                         </p>
                         {{-- <div class="bySocial col-3">
                             <ul class="social-icon-links pull-right" style="font-size: 1.5rem;">
@@ -107,126 +105,71 @@
         </div>
     </section>
     <!-- home card end -->
-    <section>
-        <div class="container mt-5 px-2">
-            <div class="row m-0">
-                <div class="col-4 d-flex justify-content-start ms-2" style="border-top: 4px dotted red">
+    @if ($blog->header)
+        <section>
+            <div class="container mt-5 px-2">
+                <div class="row m-0">
+                    <div class="col-4 d-flex justify-content-start ms-2" style="border-top: 4px dotted red">
 
+                    </div>
+                </div>
+                <div class="row m-0 px-3">
+                    <div class="col-12 d-flex justify-content-center border-top-info">
+                        <h4 class="text-center py-4">{!! $blog->header !!}</h4>
+                    </div>
+                </div>
+                <div class="row m-0">
+                    <div class="col-4 ">
+
+                    </div>
+                    <div class="col-4 ">
+
+                    </div>
+
+                    <div class="col-4" style="border-bottom: 4px dotted red">
+
+                    </div>
                 </div>
             </div>
-            <div class="row m-0 px-3">
-                <div class="col-12 d-flex justify-content-center border-top-info">
-                    <h4 class="text-center py-4">{!! $blog->header !!}</h4>
-                </div>
-            </div>
-            <div class="row m-0">
-                <div class="col-4 ">
-
-                </div>
-                <div class="col-4 ">
-
-                </div>
-
-                <div class="col-4" style="border-bottom: 4px dotted red">
-
-                </div>
-            </div>
-        </div>
-    </section>
+        </section>
+    @endif
     <section>
         <div class="container mt-5 mb-3">
             <div class="row">
                 <div class="col-lg-9 col-md-9 col-sm-12">
-                    <div class="blog_feature_description">
-                        <p>{!! $blog->short_des !!}</p>
-                    </div>
-                    <div>
-                        <div class="blog_feature_extra py-5">
-                            <p>{!! $blog->long_des !!}</p>
+                    @if ($blog->short_des)
+                        <div class="blog_feature_description">
+                            <p>{!! $blog->short_des !!}</p>
                         </div>
-                    </div>
-                    <div class="mb-5">
-                        <div class="callout m-0 p-4 text-center">
-                            <p><strong>{!! $blog->footer !!} </strong></p>
+                    @endif
+                    @if ($blog->long_des)
+                        <div>
+                            <div class="blog_feature_extra py-5">
+                                <p>{!! $blog->long_des !!}</p>
+                            </div>
                         </div>
-                    </div>
+                    @endif
+                    @if (!empty($blog->footer))
+                        <div class="mb-5">
+                            <div class="callout m-0 p-4 text-center">
+                                <p><strong>{!! $blog->footer !!} </strong></p>
+                            </div>
+                        </div>
+                    @endif
                 </div>
-                {{-- <div class="col-lg-3 col-md-3 col-sm-12 ">
-                    <div class="border my-3">
-                        <h4 class="text-center py-1 tag_title">Releted Industry</h4>
-                        <div class="btn-group d-flex flex-wrap justify-content-center">
-                            @if ($industry_ids)
-                                @foreach ($industry_ids as $item)
-                                    @php
-                                        $item = str_replace('"', '', $item);
-                                    @endphp
-                                    <a href=""
-                                        class="btn tag_btn">{{ App\Models\Admin\Industry::where('id', $item)->value('title') }}</a>
+                <div class="col-lg-3 col-md-3 col-sm-12 ">
+                    {{-- Releted Solution --}}
+                    @if (!empty($blog->tags))
+                        <div class="border my-3 ">
+                            <h4 class="text-center py-1 tag_title">TAGS</h4>
+                            <div class="text-start p-2">
+                                @foreach ($tags as $item)
+                                    <a href="" class=" text-black">#{{ $item }}</a>
                                 @endforeach
-                            @endif
+                            </div>
                         </div>
-                    </div>
-                    <div class="border my-3">
-                        <img class="img-fluid" src="https://source.unsplash.com/random/580x360" alt="">
-                    </div>
-                    <div class="border my-3">
-                        <h4 class="text-center py-1 tag_title">Releted Categories</h4>
-                        <div class="btn-group d-flex flex-wrap justify-content-center">
-                            @if ($category_ids)
-                                @foreach ($category_ids as $item)
-                                    @php
-                                        $item = str_replace('"', '', $item);
-                                    @endphp
-                                    <a href=""
-                                        class="btn tag_btn">{{ App\Models\Admin\Category::where('id', $item)->value('title') }}</a>
-                                @endforeach
-                            @endif
-                        </div>
-                    </div>
-                    <div class="border my-3">
-                        <h4 class="text-center py-1 tag_title">Releted Brand</h4>
-                        <div class="btn-group d-flex flex-wrap justify-content-center">
-                            @if ($brand_ids)
-                                @foreach ($brand_ids as $item)
-                                    @php
-                                        $item = str_replace('"', '', $item);
-                                    @endphp
-                                    <a href=""
-                                        class="btn tag_btn">{{ App\Models\Admin\Brand::where('id', $item)->value('title') }}</a>
-                                @endforeach
-                            @endif
-                        </div>
-                    </div>
-                    <div class="border my-3">
-                        <img class="img-fluid" src="https://source.unsplash.com/random/480x360" alt="">
-                    </div>
-
-                    <div class="border my-3 ">
-                        <h4 class="text-center py-1 tag_title">Releted Solution</h4>
-                        <div class="btn-group d-flex flex-wrap justify-content-center">
-                            @if ($solution_ids)
-                                @foreach ($solution_ids as $item)
-                                    @php
-                                        $item = str_replace('"', '', $item);
-                                    @endphp
-                                    <a href=""
-                                        class="btn tag_btn">{{ App\Models\Admin\SolutionDetails::where('id', $item)->value('name') }}</a>
-                                @endforeach
-                            @endif
-                        </div>
-                    </div>
-                    <div class="border my-3">
-                        <img class="img-fluid" src="https://source.unsplash.com/random/680x360" alt="">
-                    </div>
-                    <div class="border my-3 ">
-                        <h4 class="text-center py-1 tag_title">TAGS</h4>
-                        <div class="blogins_tags d-flex flex-wrap justify-content-center">
-                            @foreach ($without_last_tags as $item)
-                                <a href="" class="me-2 text-black">#{{ $item }}</a>
-                            @endforeach
-                        </div>
-                    </div>
-                </div> --}}
+                    @endif
+                </div>
             </div>
         </div>
     </section>
