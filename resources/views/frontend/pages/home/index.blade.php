9a0380448e9fd0a2e2af15ab16c88a66784bdfea
Khandker Shahed <khandker_shahed@Khandkers-Mac-mini.local>
2025-03-08 01:16:27 +0600

    23-02-25

--- resources/views/frontend/pages/home/index.blade.php
+++ resources/views/frontend/pages/home/index.blade.php
@@ -64,7 +64,7 @@
     @if (!empty(optional($home)->branner1) || !empty(optional($home)->branner2) || !empty(optional($home)->branner3))
         <section>
             <div class="row">
-                <div class="col-4" style="background: #051225">
+                <div class="col-lg-4" style="background: #051225">
                     <div class="footer_item_wrapper p-lg-6 p-4">
                         <!-- title -->
                         <h3 class="text-white" style="text-align: center;">Want To Make a <span
@@ -88,7 +88,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-8">
+                <div class="col-lg-8">
                     <div class="Advance-Slider">
                         <!-- Item -->
                         @if (!empty(optional($home)->branner1))
