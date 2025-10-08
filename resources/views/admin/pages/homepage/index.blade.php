748e8e9727f27030d483897d2de8baacb0668da1
shahed141123 <khandkershahed23@gmail.com>
2025-10-08 17:07:27 +0600

    News Trend Image size set

--- resources/views/admin/pages/homepage/index.blade.php
+++ resources/views/admin/pages/homepage/index.blade.php
@@ -3,22 +3,24 @@
         <div class="card-header mt-6">
             <div class="card-title">
             </div>
-
-            <div class="card-toolbar">
-                <a href="{{ route('admin.homepage.create') }}" class="btn btn-light-primary rounded-2">
-                    <span class="svg-icon svg-icon-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none">
-                            <rect opacity="0.3" x="2" y="2" width="20" height="20" rx="5"
-                                fill="currentColor" />
-                            <rect x="10.8891" y="17.8033" width="12" height="2" rx="1"
-                                transform="rotate(-90 10.8891 17.8033)" fill="currentColor" />
-                            <rect x="6.01041" y="10.9247" width="12" height="2" rx="1"
-                                fill="currentColor" />
-                        </svg>
-                    </span> Create
-                </a>
-            </div>
+            @if (count($homes) > 0)
+            @else
+                <div class="card-toolbar">
+                    <a href="{{ route('admin.homepage.create') }}" class="btn btn-light-primary rounded-2">
+                        <span class="svg-icon svg-icon-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none">
+                                <rect opacity="0.3" x="2" y="2" width="20" height="20" rx="5"
+                                    fill="currentColor" />
+                                <rect x="10.8891" y="17.8033" width="12" height="2" rx="1"
+                                    transform="rotate(-90 10.8891 17.8033)" fill="currentColor" />
+                                <rect x="6.01041" y="10.9247" width="12" height="2" rx="1"
+                                    fill="currentColor" />
+                            </svg>
+                        </span> Create
+                    </a>
+                </div>
+            @endif
         </div>
         <div class="card-body pt-0">
             <table class="bannersDT table align-middle table-row-dashed fs-6 gy-5 mb-0" id="kt_permissions_table">
--- resources/views/admin/pages/learnMore/index.blade.php
+++ resources/views/admin/pages/learnMore/index.blade.php
@@ -4,21 +4,24 @@
             <div class="card-title">
             </div>
 
-            <div class="card-toolbar">
-                <a href="{{ route('admin.learnmore.create') }}" class="btn btn-light-primary rounded-2">
-                    <span class="svg-icon svg-icon-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none">
-                            <rect opacity="0.3" x="2" y="2" width="20" height="20" rx="5"
-                                fill="currentColor" />
-                            <rect x="10.8891" y="17.8033" width="12" height="2" rx="1"
-                                transform="rotate(-90 10.8891 17.8033)" fill="currentColor" />
-                            <rect x="6.01041" y="10.9247" width="12" height="2" rx="1"
-                                fill="currentColor" />
-                        </svg>
-                    </span> Create
-                </a>
-            </div>
+            @if (count($learnMores) > 0)
+            @else
+                <div class="card-toolbar">
+                    <a href="{{ route('admin.learnmore.create') }}" class="btn btn-light-primary rounded-2">
+                        <span class="svg-icon svg-icon-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none">
+                                <rect opacity="0.3" x="2" y="2" width="20" height="20" rx="5"
+                                    fill="currentColor" />
+                                <rect x="10.8891" y="17.8033" width="12" height="2" rx="1"
+                                    transform="rotate(-90 10.8891 17.8033)" fill="currentColor" />
+                                <rect x="6.01041" y="10.9247" width="12" height="2" rx="1"
+                                    fill="currentColor" />
+                            </svg>
+                        </span> Create
+                    </a>
+                </div>
+            @endif
         </div>
         <div class="card-body pt-0">
             <table class="bannersDT table align-middle table-row-dashed fs-6 gy-5 mb-0" id="kt_permissions_table">
@@ -41,7 +44,8 @@
                                     {{ isset($words[$key]) ? 'Tamplate' . ' ' . Str::ucfirst($words[$key]) : 'Number out of range' }}
                                 </td>
                                 <td>
-                                    <a href="{{ route('admin.learnmore.edit', [$learnMore->id]) }}" class="text-primary">
+                                    <a href="{{ route('admin.learnmore.edit', [$learnMore->id]) }}"
+                                        class="text-primary">
                                         <i class="fas fa-pen me-3 dash-icons text-primary"></i>
                                     </a>
                                     <a href="{{ route('admin.learnmore.destroy', [$learnMore->id]) }}"
